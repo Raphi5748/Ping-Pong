343e4cf69d2bb51bc1e7886b4e45a6ac38bb2f9c
Raphi5748 <132646550+Raphi5748@users.noreply.github.com>
2023-07-01 13:53:50 +0200

    Farblich Wechselbare Kugel

--- Ping_Pong.py
+++ Ping_Pong.py
@@ -9,6 +9,7 @@ uhr = pygame.time.Clock()
 Pause = True
 spiel_Aktiv = True
 EinzelSpieler = False
+customicer = False
 vzähler = 1
 AktuellerSpielModus = "Zweispieler"
 vx = 2
@@ -24,7 +25,7 @@ plusminus = [-1,1]
 #Musik
 
 pygame.mixer.music.load('514154__edwardszakal__game-music.mp3')
-pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.set_volume(0.0)
 pygame.mixer.music.play(-1)
 
 #Spielfeld,Bilder und Steuerungselemente
@@ -54,11 +55,49 @@ x = BREITE / 6 * 5
 y = HOEHE / 2
 Balken2_rechteck = Balken2.get_rect(center = (x,y))
 
-Kugel = pygame.image.load('KugelPingPong.png')
+OrangenerBallBild = "OrangenerBall.png"
+PinkerBallBild = "PinkerBall.png"
+TürkiserBallBild = "TürkiserBall.png"
+GrünerBallBild = "GrünerBall.png"
+GelberBallBild = "GelberBall.png"
+
+AktiverBall = GrünerBallBild
+
+Kugel = pygame.image.load(AktiverBall)
 Kugel = pygame.transform.scale(Kugel, (100,100))
 x = BREITE / 2
 y = HOEHE / 2
 Kugel_rechteck = Kugel.get_rect(center = (x,y))
+
+OrangenerBall = pygame.image.load(OrangenerBallBild)
+OrangenerBall = pygame.transform.scale(OrangenerBall, (100,100))
+x = BREITE / 6
+y = HOEHE / 2
+OrangenerBall_rechteck = OrangenerBall.get_rect(center = (x,y))
+
+PinkerBall = pygame.image.load(PinkerBallBild)
+PinkerBall = pygame.transform.scale(PinkerBall, (100,100))
+x = BREITE / 6 * 2
+y = HOEHE / 2
+PinkerBall_rechteck = PinkerBall.get_rect(center = (x,y))
+
+TürkiserBall = pygame.image.load(TürkiserBallBild)
+TürkiserBall = pygame.transform.scale(TürkiserBall, (100,100))
+x = BREITE / 6 * 3
+y = HOEHE / 2
+TürkiserBall_rechteck = TürkiserBall.get_rect(center = (x,y))
+
+GrünerBall = pygame.image.load(GrünerBallBild)
+GrünerBall = pygame.transform.scale(GrünerBall, (100,100))
+x = BREITE / 6 * 4
+y = HOEHE / 2
+GrünerBall_rechteck = GrünerBall.get_rect(center = (x,y))
+
+GelberBall = pygame.image.load(GelberBallBild)
+GelberBall = pygame.transform.scale(GelberBall, (100,100))
+x = BREITE / 6 * 5
+y = HOEHE / 2
+GelberBall_rechteck = GelberBall.get_rect(center = (x,y))
 
 PauseBild = pygame.image.load('Pausezeichen.png')
 PauseBild = pygame.transform.scale(PauseBild, (400,100))
@@ -72,17 +111,32 @@ x = BREITE / 4
 y = HOEHE / 2
 Steuerung_rechteck = Steuerung.get_rect(center = (x,y))
 
+Einkaufstasche = pygame.image.load('Einkaufstasche.png')
+Einkaufstasche = pygame.transform.scale(Einkaufstasche, (100,100))
+x = BREITE / 6 * 5
+y = HOEHE / 5
+Einkaufstasche_rechteck = Einkaufstasche.get_rect(center = (x,y))
+
+Fortsetzen = pygame.image.load('Fortsetzen.png')
+Fortsetzen = pygame.transform.scale(Fortsetzen, (200,100))
+x = BREITE / 2
+y = HOEHE / 5 * 4
+Fortsetzen_rechteck = Fortsetzen.get_rect(center = (x,y))
+
 Punktestand1 = Punktestand_Font.render(str(punktzahls1), False, (0, 0, 0))
 
 fenster.blit(background, (0,0))
 fenster.blit(Balken,Balken_rechteck)
 fenster.blit(Balken2,Balken2_rechteck)
 fenster.blit(Kugel,Kugel_rechteck)
+fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
+
 
 #Spiel
 
 while spiel_Aktiv:
     
+
     # Zeitabhänige Geschwindigkeitserhöhung
     vzähler += 1
     if vzähler == 120:
@@ -132,11 +186,51 @@ while spiel_Aktiv:
                 spiel_aktiv = False
                 pygame.quit()
                 sys.exit()
+            mp = pygame.mouse.get_pos()
+            if Einkaufstasche_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                customicer = True
+            while customicer == True:
+                for event in pygame.event.get():
+                    print(customicer)
+                    if event.type == pygame.QUIT:
+                        spiel_aktiv = False
+                        pygame.quit()
+                        sys.exit()
+                        
+                    fenster.blit(background, (0,0))
+                    fenster.blit(OrangenerBall, OrangenerBall_rechteck)
+                    fenster.blit(PinkerBall, PinkerBall_rechteck)
+                    fenster.blit(TürkiserBall, TürkiserBall_rechteck)
+                    fenster.blit(GrünerBall, GrünerBall_rechteck)
+                    fenster.blit(GelberBall, GelberBall_rechteck)
+                    fenster.blit(Fortsetzen, Fortsetzen_rechteck)
+                    fenster.blit(Kugel, (BREITE / 5, HOEHE / 7))
+                    uhr.tick(120)
+                    pygame.display.update()
+                    tastatur = pygame.key.get_pressed()
+                    mp = pygame.mouse.get_pos()
+                    if Fortsetzen_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                        customicer = False
+                    if OrangenerBall_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                        AktiverBall = OrangenerBallBild
+                    if PinkerBall_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                        AktiverBall = PinkerBallBild
+                    if TürkiserBall_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                        AktiverBall = TürkiserBallBild
+                    if GelberBall_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                        AktiverBall = GelberBallBild
+                    if GrünerBall_rechteck.collidepoint(mp) and event.type == pygame.MOUSEBUTTONDOWN:
+                        AktiverBall = GrünerBallBild
+                        
+                    Kugel = pygame.image.load(AktiverBall)
+                    Kugel = pygame.transform.scale(Kugel, (100,100))
+                    
             # Pausemenü anzeigen
+            fenster.blit(background, (0,0))
             fenster.blit(PauseBild, Pause_rechteck)
             fenster.blit(Steuerung, Steuerung_rechteck)
             fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2))
-
+            fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
             # Steuerung Pausemenü
             tastatur = pygame.key.get_pressed()
             if tastatur[pygame.K_SPACE] == 1:
@@ -147,14 +241,9 @@ while spiel_Aktiv:
                 EinzelSpieler = True
                 AktuellerSpielModus = "Einzelspieler"
                 SpielModusText = Punktestand_Font.render(str(AktuellerSpielModus), False, (0, 0, 0))
-                
                 fenster.blit(background, (0,0))
-                fenster.blit(Balken,Balken_rechteck)
-                fenster.blit(Balken2,Balken2_rechteck)
-                fenster.blit(Kugel,Kugel_rechteck)
-                fenster.blit(Punktestand1, (Balken_rechteck.centerx, HOEHE / 8))
                 fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2))
-                
+                fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
             if tastatur[pygame.K_LEFT] == 1:
                 # Zweispieler Aktivieren
                 EinzelSpieler = False
@@ -162,41 +251,29 @@ while spiel_Aktiv:
                 SpielModusText = Punktestand_Font.render(str(AktuellerSpielModus), False, (0, 0, 0))
                 
                 fenster.blit(background, (0,0))
-                fenster.blit(Balken,Balken_rechteck)
-                fenster.blit(Balken2,Balken2_rechteck)
-                fenster.blit(Kugel,Kugel_rechteck)
-                fenster.blit(Punktestand1, (Balken_rechteck.centerx, HOEHE / 8))
                 fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2))
+                fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
             if tastatur[pygame.K_1] == 1:
                 AktivesBild = Bild1
                 background = pygame.image.load(AktivesBild)
                 background = pygame.transform.scale(background, (BREITE,HOEHE))
                 fenster.blit(background, (0,0))
-                fenster.blit(Balken,Balken_rechteck)
-                fenster.blit(Balken2,Balken2_rechteck)
-                fenster.blit(Kugel,Kugel_rechteck)
-                fenster.blit(Punktestand1, (Balken_rechteck.centerx, HOEHE / 8))
                 fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2))
+                fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
             if tastatur[pygame.K_2] == 1:
                 AktivesBild = Bild2
                 background = pygame.image.load(AktivesBild)
                 background = pygame.transform.scale(background, (BREITE,HOEHE))
                 fenster.blit(background, (0,0))
-                fenster.blit(Balken,Balken_rechteck)
-                fenster.blit(Balken2,Balken2_rechteck)
-                fenster.blit(Kugel,Kugel_rechteck)
-                fenster.blit(Punktestand1, (Balken_rechteck.centerx, HOEHE / 8))
                 fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2))
+                fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
             if tastatur[pygame.K_3] == 1:
                 AktivesBild = Bild3
                 background = pygame.image.load(AktivesBild)
                 background = pygame.transform.scale(background, (BREITE,HOEHE))
                 fenster.blit(background, (0,0))
-                fenster.blit(Balken,Balken_rechteck)
-                fenster.blit(Balken2,Balken2_rechteck)
-                fenster.blit(Kugel,Kugel_rechteck)
-                fenster.blit(Punktestand1, (Balken_rechteck.centerx, HOEHE / 8))
-                fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2)) 
+                fenster.blit(SpielModusText, (BREITE / 6 * 4, HOEHE / 2))
+                fenster.blit(Einkaufstasche, Einkaufstasche_rechteck)
             uhr.tick(120)
             pygame.display.update()
     # X und Y bewegung 
